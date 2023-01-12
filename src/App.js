fef211d88f86d914526c30b247b0976df57b7b44
arezou-r <arezou.ruhi@gmail.com>
2023-01-12 13:40:09 +0330

    Added github link

--- src/App.js
+++ src/App.js
@@ -4,7 +4,11 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      Weather app
+      <h2>Weather app</h2>
+      <footer>
+        This project was coded by Arezou Ruohi and is 
+        <a href='https://github.com/arezou-r/weather-app-react' target="_blank" rel="noreferer noreferrer"> open-sourced on Github</a>
+      </footer>
     </div>
   );
 }
